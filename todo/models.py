a626a8d000f50cfdf1f88e2fef102ea64ba53834
Bruno Slamek <bslamek@idc.com>
2022-01-19 12:00:02 -0300

    +page index and list items

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class ToDo(models.Model):
+
+    choices_priority = [
+        ('Urgent', 'Urgent'),
+        ('Medium', 'Medium'),
+        ('Later', 'Later'),
+    ]
+
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    priority = models.CharField(max_length=6, choices=choices_priority)
+    is_active = models.BooleanField()
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title} + {self.priority}'
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import ToDo
 
-
+"""
 def index(request):
     return render(request, 'index.html')
+"""
+
+
+class IndexView(ListView):
+    models = ToDo
+    template_name = 'index.html'
+    queryset = ToDo.objects.all()
+    context_object_name = 'todos'
